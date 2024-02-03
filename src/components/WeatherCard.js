457b71181d7d131f527882407f2f9827fba42ed9
Kvirike kemularia <kvirikekemularia@gmail.com>
2024-02-03 23:28:24 +0400

    finishing functionality
    getting all weather info from API and passing it to the element

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -14,17 +14,18 @@ function WeatherCard({location, current }) {
                                     <h6>{location.name}</h6>
                                 </div>
                                 <div className="d-flex flex-column text-center mt-5 mb-4">
-                                    <h6 className="display-4 mb-0 font-weight-bold" style={{ color: '#1C2331' }}> 13°C </h6>
-                                    <span className="small" style={{ color: '#868B94' }}>Stormy</span>
+                                    <h6 className="display-4 mb-0 font-weight-bold" style={{ color: '#1C2331' }}>{current.temp_c}º</h6>
+                                    <span className="small" style={{ color: '#868B94' }}>{current.condition.text}</span>
                                 </div>
                                 <div className="d-flex align-items-center">
                                     <div className="flex-grow-1" style={{ fontSize: '1rem' }}>
-                                        <div><i className="fas fa-wind fa-fw" style={{ color: '#868B94' }}></i> <span className="ms-1"> 40 km/h </span></div>
-                                        <div><i className="fas fa-tint fa-fw" style={{ color: '#868B94' }}></i> <span className="ms-1"> 84% </span></div>
-                                        <div><i className="fas fa-sun fa-fw" style={{ color: '#868B94' }}></i> <span className="ms-1"> 0.2h </span></div>
+                                        <div><i className="fas fa-wind fa-fw" style={{ color: '#868B94' }}></i> <span className="ms-1">Time: {location.localtime}</span></div>
+                                        <div><i className="fas fa-wind fa-fw" style={{ color: '#868B94' }}></i> <span className="ms-1">Wind: {current.wind_kph} km/h</span></div>
+                                        <div><i className="fas fa-tint fa-fw" style={{ color: '#868B94' }}></i> <span className="ms-1">Humidity: {current.humidity}%</span></div>
+                                        <div><i className="fas fa-sun fa-fw" style={{ color: '#868B94' }}></i> <span className="ms-1"></span></div>
                                     </div>
                                     <div>
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu1.webp" width="100px" alt="weather-icon" />
+                                        <img src={current.condition.icon} width="100px" alt="weather-icon" />
                                     </div>
                                 </div>
                             </div>
